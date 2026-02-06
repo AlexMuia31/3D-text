dd26de6f0de63dcd906d8406bb12ac04f0574123
AlexMuia31 <alexmuia31@gmail.com>
2026-02-07 02:31:59 +0300

    added font to Text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,13 @@ export const Experience = () => {
     <>
       <group position-x={-1.5} rotation-y={THREE.MathUtils.degToRad(15)}>
         <primitive object={woodenSign.scene} />
-        <Text>
+        <Text
+          fontSize={0.3}
+          position={[0, 1.2, 0.01]}
+          maxWidth={1}
+          textAlign="center"
+          font="fonts/PlaywriteNZBasic-VariableFont_wght.ttf"
+        >
           Alex Muia <meshStandardMaterial color="#000" />
         </Text>
       </group>
