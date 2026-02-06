e8aa75f308f874f1847f2281c74b17bb28617e07
AlexMuia31 <alexmuia31@gmail.com>
2026-02-07 01:59:58 +0300

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,23 @@
+import { ContactShadows, Text, useGLTF } from "@react-three/drei";
+import { Character } from "./Character";
+
+import * as THREE from "three";
+
+export const Experience = () => {
+  const woodenSign = useGLTF("models/Wooden Sign.glb");
+
+  return (
+    <>
+      <group position-x={-1.5} rotation-y={THREE.MathUtils.degToRad(15)}>
+        <primitive object={woodenSign.scene} />
+        <Text>
+          Alex Muia <meshStandardMaterial color="#000" />
+        </Text>
+      </group>
+      <group position={[1.5, 0, 0]} rotation-y={-Math.PI / 4}>
+        <Character />
+      </group>
+      <ContactShadows opacity={0.42} scale={42} far={42} />
+    </>
+  );
+};
