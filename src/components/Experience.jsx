e5250f83d2bb8782ceb80b20cc44d71d7c5d3323
AlexMuia31 <alexmuia31@gmail.com>
2026-02-10 13:33:26 +0300

    added bevel to text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,10 @@
-import { Billboard, ContactShadows, Text, useGLTF } from "@react-three/drei";
+import {
+  Billboard,
+  ContactShadows,
+  Text,
+  Text3D,
+  useGLTF,
+} from "@react-three/drei";
 import { Character } from "./Character";
 
 import * as THREE from "three";
@@ -8,6 +14,18 @@ export const Experience = () => {
 
   return (
     <>
+      <Text3D
+        font="fonts/Inter_Bold.json"
+        rotation-y={THREE.MathUtils.degToRad(30)}
+        size={4}
+        position={[-8, 0, -5]}
+        bevelEnabled
+        bevelThickness={0.5}
+        bevelSize={0.1}
+      >
+        Alex
+        <meshStandardMaterial color={"#a1bb6f"} />
+      </Text3D>
       <group position-x={-1.5} rotation-y={THREE.MathUtils.degToRad(15)}>
         <primitive object={woodenSign.scene} />
         <Text
